d52e76506227bb968b7bcd8f9bde2a6b42e1df87
Joseph D. Puerari <joep@Joes-MBP.fios-router.home>
2020-04-23 14:09:14 -0400

    title

---
+++ src/components/Portfolio.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const name = "Joseph D. Puerari";
+
+function Portfolio() {
+  return (
+    <h1> Welcome to {name}'s Coding Portfolio!</h1>
+  );
+}
+
+export default Portfolio;
