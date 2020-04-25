2df274440d8b8ac02fcc09f492a4eac32be4f871
Joseph D. Puerari <joep@Joes-MBP.fios-router.home>
2020-04-25 12:13:34 -0400

    finalized for now

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Portfolio from './components/Portfolio';
 import Main from './components/Main';
-import Links from './components/Links';
 import Stylesheet from './components/stylesheet';
 
 
@@ -10,7 +9,6 @@ function App() {
     <div>
   <Portfolio />
   <Main />
-  <Links />
   <Stylesheet />
   </div>
   )
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
+
+
+const links = ['GitHub Profile: https://github.com/jpuerari', 'LinkedIn Profile: https://www.linkedin.com/in/josephdpuerari/']
+
+
 function Main() {
   return (
-  
-      <h1 className='secondary'>Here are my portfolio links</h1>
-    
+    <div h1 className='secondary'>
+      <h1>Here are my portfolio links</h1>
+
+      <p className='tertiary'>{links[0]}</p>
+      <p className='tertiary'>{links[1]}</p>
+
+
+
+      <div class="LI-profile-badge" data-version="v1" data-size="medium" data-locale="en_US" data-type="vertical" data-theme="dark" data-vanity="josephdpuerari"><a class="LI-simple-link" href='https://www.linkedin.com/in/josephdpuerari?trk=profile-badge'>Joseph P.</a></div>
+
+    </div>
   );
 }
 
